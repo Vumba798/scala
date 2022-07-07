c71e2279566865aa2f3db46b1b29b6b74ee1fde4
Vumba798 <alexandrov32649@gmail.com>
2022-07-08 00:28:42 +0300

    exr 4 hard

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,10 @@
 import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
+import redBookExercises._
 
 object Main extends App:
-  val arr = Array(1, 5, 3, 4)
-  println(isSorted(arr, (x, y) => x < y))
+
+
 end Main
 
 
---
+++ src/main/scala/redBookExercises/firstChapter.scala
@@ -0,0 +1,18 @@
+package redBookExercises
+
+import scala.annotation.tailrec
+
+
+def isSorted[T](d: Array[T], f: (T, T) => Boolean): Boolean =
+  @tailrec
+  def go(head: T, tail: Array[T]): Boolean =
+    if tail.isEmpty then true
+    else if f(head, tail.head) then go(tail.head, tail.tail)
+    else false
+
+  go(d.head, d.tail)
+
+def partiall[A, B, C](a: A, f: (A, B) => C): B => C = (b: B) => f(a, b)
+
+def curry[A, B, C](f: (A, B) => C): A => B => C =
+  (a: A) => (b: B) => f(a, b)
