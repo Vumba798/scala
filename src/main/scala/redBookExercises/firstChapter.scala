09403d6901525738b234dae95145c85daeb6ac05
Vumba798 <alexandrov32649@gmail.com>
2022-07-08 00:30:42 +0300

    exr 5 optional

--- src/main/scala/redBookExercises/firstChapter.scala
+++ src/main/scala/redBookExercises/firstChapter.scala
@@ -16,3 +16,6 @@ def partiall[A, B, C](a: A, f: (A, B) => C): B => C = (b: B) => f(a, b)
 
 def curry[A, B, C](f: (A, B) => C): A => B => C =
   (a: A) => (b: B) => f(a, b)
+
+def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+  (a: A, b: B) => f(a)(b)
