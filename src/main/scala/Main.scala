d949fe9c102f7d73b3bf8b088387727863be29de
Vumba798 <alexandrov32649@gmail.com>
2022-07-09 02:44:14 +0300

    solved few more tasks

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,9 +4,8 @@ import redBookExercises.*
 import redBookExercises.Chapter3._
 
 object Main extends App:
-  val l = Cons(-3, Cons(-2, Cons(-1, Cons(0, Cons(1, Nil)))))
-  val dropWhileNegative = dropWhile((x: Int) => x < 0)
-  println(dropWhileNegative(l))
+  val l = Cons(1, Cons(2, Cons(3, Nil)))
+  println(length(l))
 end Main
 
 
--- src/main/scala/redBookExercises/Chapter3/Chapter3.scala
+++ src/main/scala/redBookExercises/Chapter3/Chapter3.scala
@@ -1,5 +1,7 @@
 package redBookExercises.Chapter3
 
+import scala.annotation.tailrec
+
 sealed trait List[+A]
 
 case object Nil extends List[Nothing]
@@ -13,7 +15,6 @@ object List {
 
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
-    case Cons(0.0, _) => 0
     case Cons(x, xs) => x * product(xs)
   }
 
@@ -45,4 +46,51 @@ def dropWhile[A](f: A => Boolean)(xs: List[A]): List[A] = xs match {
   case Nil => Nil
   case Cons(a, as) if f(a) => dropWhile(f)(as)
   case _ => xs
+}
+
+// exr 5
+def setHead[A](l: List[A], newHead: A) = Cons(newHead, tail(l))
+
+def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+  case Nil => a2
+  case Cons(h, t) => Cons(h, append(t, a2))
+}
+
+// exr 6
+def init[A](l: List[A]): List[A] = l match {
+  case Nil => Nil
+  case Cons(h, Nil) => Nil
+  case Cons(h, t) => Cons(h, init(t))
+}
+
+def foldRight[A, B](l: List[A], z: B)(f: (A, B) => B): B = l match {
+  case Nil => z
+  case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+}
+
+def sum2(l: List[Int]) = foldRight(l, 0)(_ + _)
+def prod2(l: List[Double]) = foldRight(l, 1.0)(_ * _)
+
+// exr 8
+val exr8 = foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _)) // returns List(1, 2, 3)
+
+// exr 9
+def length[A](l: List[A]): Int = foldRight(l, 0)((_, lenAcc: Int) => lenAcc + 1)
+
+// exr 10
+@tailrec
+def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+  case Cons(h, t) => foldLeft(t, f(z, h))(f)
+  case Nil => z
+}
+
+// exr 11
+def sum3(l: List[Int]): Int = foldLeft(l, 0)(_ + _)
+def product3(l: List[Double]): Double = foldLeft(l, 1.0)(_ * _)
+def length3[A](l: List[A]): Int = foldLeft(l, 0)((z, _) => z + 1)
+
+// exr 12
+def reverse1[A](l: List[A]): List[A] = l match {
+  case Cons(x, xs) => foldLeft(xs, Cons(x, Nil))((z, x) => Cons(x, z))
+  case Nil => Nil
 }
