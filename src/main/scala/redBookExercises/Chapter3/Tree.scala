f14207f3cf6dbea097d0b1960b668af71416f81a
Vumba798 <alexandrov32649@gmail.com>
2022-07-10 13:19:54 +0300

    solved all tasks in chapter 3

--- src/main/scala/redBookExercises/Chapter3/Tree.scala
+++ src/main/scala/redBookExercises/Chapter3/Tree.scala
@@ -20,9 +20,23 @@ object Tree {
   // exr 27
   def depth[A](t: Tree[A]): Int = t match {
     case Leaf(_) => 1
-    case Branch(l, r) => 1 + (depth(l))
+    case Branch(l, r) => 1 + depth(l)
   }
 
+  // exr 28
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
 
+  // exr 29
+  def fold[A, B](t: Tree[A])(f: A => B)(op: (B, B) => B): B = t match {
+    case Branch(l, r) => op(fold(l)(f)(op), fold(r)(f)(op))
+    case Leaf(x) => f(x)
+  }
+
+  def size1[A](t: Tree[A]): Int = fold(t)(x => 1)(_ + _)
+  def maximum1(t: Tree[Int]): Int = fold(t)(x => x)(_ max _)
+  def depth1[A](t: Tree[A]): Int = fold(t)(x => 1)(1 + _ max _)
 }
 
