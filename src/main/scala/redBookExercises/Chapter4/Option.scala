689ad529d4881b8a7142715bfb7e93574e8d90f8
Vumba798 <alexandrov32649@gmail.com>
2022-07-10 23:59:58 +0300

    defined Option trait signatures

---
+++ src/main/scala/redBookExercises/Chapter4/Option.scala
@@ -0,0 +1,17 @@
+package redBookExercises.Chapter4
+
+trait Option[+A] {
+  def map[B](f: A => B): Option[B]
+  def flatMap[B](f: A => Option[B]): Option[B]
+  def getOrElse[B >: A](default: => B): B
+  def orElse[B >: A](ob: => Option[B]): Option[B]
+  def filter(f: A => Boolean): Option[A]
+}
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+}
